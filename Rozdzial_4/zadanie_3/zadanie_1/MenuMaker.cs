72dbc908d691698e9e31a757595ec94867054906
marcingrotowski <marcingrotowski@outlook.com>
2022-10-25 15:50:52 +0200

    25.10.2022 poprawka

---
+++ Rozdzial_4/zadanie_3/zadanie_1/MenuMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie_1
+{
+    internal class MenuMaker
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "Pieczona wołowina", "Salami", "Indyk", "Szynka", "Karkówka" };
+        string[] Condiments = { "żółta musztarda", "brązowa musztarda", "musztarda miodowa", "majonez", "przyprawa", "sos francuski" };
+        string[] Breads = { "chleb ryżowy", "chleb biały", "chleb zbożowy", "pumpernikiel", "chleb włoski", "bułka" };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiments = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + ", " + randomCondiments + ", " + randomBread;
+        }
+    }
+}
