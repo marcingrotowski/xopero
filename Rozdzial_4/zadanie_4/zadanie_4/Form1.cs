23f67b61f971068a44b8a20e690f7d86666ba18b
marcingrotowski <marcingrotowski@outlook.com>
2022-10-26 09:29:59 +0200

    26.10.2022 + poprawki

--- Rozdzial_4/zadanie_4/zadanie_4/Form1.cs
+++ Rozdzial_4/zadanie_4/zadanie_4/Form1.cs
@@ -2,6 +2,9 @@ namespace zadanie_4
 {
     public partial class Form1 : Form
     {
+
+        Random random = new Random();
+        Stats stats = new Stats();
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +12,44 @@ namespace zadanie_4
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Koniec gry");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode)) {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                {
+                    timer1.Interval -= 10;
+                }
+                if (timer1.Interval > 250)
+                {
+                    timer1.Interval -= 7;
+                }
+                if (timer1.Interval > 100)
+                {
+                    timer1.Interval -= 2;
+                }
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+                stats.Update(true);
+            }
+            else
+            {
+                stats.Update(false);
+            }
+
+            correctLabel.Text = "Prawid³owych: " + stats.Correct;
+            missedLabel.Text = "B³êdnych: " + stats.Missed;
+            totalLabel.Text = "Wszystkich: " + stats.Total;
+            accuracyLabel.Text = "Dok³adnoœæ: " + stats.Accuracy;
         }
     }
 }
---
+++ Rozdzial_4/zadanie_5/zadanie_5/LabelBouncer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie_5
+{
+    internal class LabelBouncer
+    {
+        public Label MyLabel;
+        public bool GoingForward;
+        public void Move()
+        {
+            if(MyLabel != null)
+            {
+                if (GoingForward)
+                {
+                    MyLabel.Left += 5;
+                    if (MyLabel.Left >= MyLabel.Parent.Width - MyLabel.Width)
+                    {
+                        GoingForward = false;
+                    }
+                }
+                else
+                {
+                    MyLabel.Left -= 5;
+                    if(MyLabel.Left <= 0)
+                    {
+                        GoingForward = true;
+                    }
+                }
+            }
+        }
+    }
+}
