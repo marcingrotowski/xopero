b5675595ad02daf983850e883eaf3e39b589f2cf
marcingrotowski <marcingrotowski@outlook.com>
2022-11-10 12:11:27 +0100

    10.11.2022

--- Rozdzial_8/ćwiczenie_listy_kaczki/ćwiczenie_listy_kaczki/Program.cs
+++ Rozdzial_8/ćwiczenie_listy_kaczki/ćwiczenie_listy_kaczki/Program.cs
@@ -1,20 +1,25 @@
 ﻿using ćwiczenie_listy_kaczki;
 
-static void Main(string[] args) {
-    List<Duck> ducks = new List<Duck>()
-    {
-    new Duck(){Kind = KindOfDuck.Mallard, Size = 17},
-    new Duck(){Kind = KindOfDuck.Muscovy, Size = 18},
-    new Duck(){Kind = KindOfDuck.Decoy, Size = 14},
-    new Duck(){Kind = KindOfDuck.Muscovy, Size = 11},
-    new Duck(){Kind = KindOfDuck.Mallard, Size = 14},
-    new Duck(){Kind = KindOfDuck.Decoy, Size = 13},
-    };
+namespace ćwiczenie_listy_kaczki { 
 
-    static void PrintDucks(List<Duck> ducks)
+    class Program
     {
-        foreach (Duck duck in ducks)
-            Console.WriteLine(duck.Size.ToString() + "-centrymetrowa kaczka " + duck.Kind.ToString());
-        Console.WriteLine("Koniec kaczek!");
+        static void Main(string[] args) {
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(){Kind = KindOfDuck.Mallard, Size = 17},
+                new Duck(){Kind = KindOfDuck.Muscovy, Size = 18},
+                new Duck(){Kind = KindOfDuck.Decoy, Size = 14},
+                new Duck(){Kind = KindOfDuck.Muscovy, Size = 11},
+                new Duck(){Kind = KindOfDuck.Mallard, Size = 14},
+                new Duck(){Kind = KindOfDuck.Decoy, Size = 13},
+            };
+            static void PrintDucks(List<Duck> ducks)
+            {
+            foreach (Duck duck in ducks)
+                Console.WriteLine(duck.Size.ToString() + "-centrymetrowa kaczka " + duck.Kind.ToString());
+            Console.WriteLine("Koniec kaczek!");
+            }
+        }
     }
 }
